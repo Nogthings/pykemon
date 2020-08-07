8df7d50c9eddd1c04c6239364200714eab5eb18d
Nogthings <gcoscar99@yahoo.com>
2020-08-07 15:36:26 -0600

    bugs ask_command fixed

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from constants *
-from models *
+from constants import *
+from models import *
 
-#Firs, define pokemons with stats
+#First define pokemons with its stats
 
 pokemon1 = Pokemon("Bulbasaur", 100, "grass", "poison")
-pokemon2 = Pokemon("Charmander", 100, "fire", none)
+pokemon2 = Pokemon("Charmander", 100, "fire", None)
 pokemon1.current_hp = 45
 pokemon2.current_hp = 39
+#Stats
 
-#stats
 pokemon1.stats = {
     HP: 45,
     ATTACK: 49,
-    DEFFENSE: 49,
+    DEFENSE: 49,
     SPATTACK: 65,
     SPDEFENSE: 65,
     SPEED: 45
@@ -21,42 +21,46 @@ pokemon1.stats = {
 pokemon2.stats = {
     HP: 39,
     ATTACK: 52,
-    DEFFENSE: 43,
+    DEFENSE: 43,
     SPATTACK: 80,
     SPDEFENSE: 65,
     SPEED: 65
 }
 
-#attacks
-pokemon1.attacks [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
-pokemon2.attacks [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
+#Attacks
+pokemon1.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
+pokemon2.attacks = [Attack("scratch", "normal", PHYSICAL, 10, 10, 100)]
 
-#start-Battle
-battle = Battle (pokemon1)
 
-    def ask_command(pokemon1):
-        command = None
-        while not command:
-            #DO ATTACK -> attack 0
-            tmp_command = input ("What should"+ pokemon.name + "do?").split(" ")
-            if len(tmp_command) == 2:
-                try:
-                    if tmp_command[0] == DO_ATTACK and 0 <= int(tmp_command[1]) < 4
-                        command = Command ({DO_ATTACK: (int(tmp_command[1]) })
-                except Exception:
-                    pass
-        return command
+def ask_command(pokemon):
+    command = None
+    while not command:
+        tmp_command = input("What should "+pokemon.name+" do?").split(" ")
+        if len(tmp_command) == 2:
+            try:
+                if tmp_command[0] == DO_ATTACK and 0 <= int(tmp_command[1]) < 4:
+                    command = Command({DO_ATTACK: int(tmp_command[1])})
+            except Exception:
+                pass
+    return command
 
-    while not battle.is_finished():
-        #first ask for command
-        command1 = ask_command(pokemon1)
-        command2 = ask_command(pokemon2)
+#Start battle
 
-        turn = Turn()
-        turn.command1 = command1
-        turn.command2 = command2
+battle = Battle(pokemon1, pokemon2)
 
-        if turn.can_start():
-            #execute turn
-            battle.execute_turn(turn)
-            Battle.print_current_status()
+while not battle.is_finished():
+    #Main pokemon battle loop
+    #First ask for the commands
+    command1 = ask_command(pokemon1)
+    command2 = ask_command(pokemon2)
+
+    #Generate new turn
+    turn = Turn()
+    turn.command1 = command1
+    turn.command2 = command2
+
+    if turn.can_start():
+        # Execute turn
+        battle.execute_turn(turn)
+        battle.print_current_status()
+
--- models.py
+++ models.py
@@ -1,61 +1,64 @@
 from constants import *
 
+
 class Battle:
-    def __init__ (self, pokemon1, pokemon2):
+
+    def __init__(self, pokemon1, pokemon2):
         self.pokemon1 = pokemon1
         self.pokemon2 = pokemon2
         self.actual_turn = 0
 
     def is_finished(self):
-        finished = self.pokemon1.current_hp <=0 or self.pokemon2.current_hp <=0
+        finished = self.pokemon1.current_hp <= 0 or self.pokemon2.current_hp <= 0
         if finished:
             self.print_winner()
         return finished
 
+    def print_winner(self):
+        if self.pokemon1.current_hp <= 0 < self.pokemon2.current_hp:
+                print(self.pokemon2.name + " won in " + str(self.actual_turn)+" turns!!")
+        elif self.pokemon2.current_hp <= 0 < self.pokemon1.current_hp:
+                print(self.pokemon1.name + " won in " + str(self.actual_turn)+" turns!!")
+        else:
+            print("DOUBLE KO!")
+
     def execute_turn(self, turn):
+        # Logic to execute a turn inside a battle
         command1 = turn.command1
         command2 = turn.command2
         attack1 = None
         attack2 = None
-
         if DO_ATTACK in command1.action.keys():
             attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
         if DO_ATTACK in command2.action.keys():
             attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
 
-        #damage formula
+        # Execute attacks
         self.pokemon2.current_hp -= attack1.power
         self.pokemon1.current_hp -= attack2.power
-
         self.actual_turn += 1
 
-        def print_current_status (self):
-            print(self.name.pokemon1 + "has " + str(self.pokemon1.current_hp) + " left")
-            print(self.name.pokemon2 + "has " + str(self.pokemon2.current_hp) + " left")
-
-        def print_winner (self):
-            if self.pokemon1.current_hp <= 0 and self.pokemon2.current_hp > 0
-                print(self.pokemon2.name + " won in " str(self.actual_turn) + " turns!")
-            elif self.pokemon2.current_hp <= 0 and self.pokemon1.current_hp > 0
-                print(self.pokemon1.name + " won in " str(self.actual_turn) + " turns!")
-            else:
-                print ("Double K.O!")
+    def print_current_status(self):
+        print(self.pokemon1.name + " has " + str(self.pokemon1.current_hp) + " left!")
+        print(self.pokemon2.name + " has " + str(self.pokemon2.current_hp) + " left!")
 
 
-class Pokemon :
-    def __init__ (self, name, level, type1, type2):
-        self.name = name
-        self.level = level
-        self.type1 = type1
+class Pokemon:
+
+    def __init__(self, name, level, type1, type2):
         self.type2 = type2
+        self.type1 = type1
+        self.level = level
+        self.name = name
         self.attacks = []
         self.stats = {}
         self.current_status = 0
         self.current_hp = 0
 
+
 class Attack:
 
-    def __init__ (self, name, t, category, pp, power, accuracy):
+    def __init__(self, name, t, category, pp, power, accuracy):
         self.name = name
         self.type = t
         self.category = category
@@ -63,13 +66,18 @@ class Attack:
         self.power = power
         self.accuracy = accuracy
 
+
 class Turn:
+
     def __init__(self):
         self.command1 = None
         self.command2 = None
-    def can_start(self)
+
+    def can_start(self):
         return self.command1 is not None and self.command2 is not None
 
+
 class Command:
+
     def __init__(self, action):
         self.action = action
